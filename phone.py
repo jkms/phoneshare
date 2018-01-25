42b1ea2f1f44bdbf95fe6b6d99397ecb24d4b9c5
John Stafford <john@phone.thebug.life>
2018-01-25 17:00:21 +0000

    code cleanup

--- phone.py
+++ phone.py
@@ -16,26 +16,12 @@ r.hset(m.hexdigest(), 'name', 'Jenny')
 @app.route('/')
 def hello_world():
 	return 'Hello World!'
-numbers = [
-    {
-        'id': 1,
-        'e164': u'+16048675309',
-    },
-    {
-        'id': 2,
-        'e165': u'+16045555555',
-    }
-]
 
-@app.route('/api/v2.0/numbers', methods=['GET'])
+@app.route('/api/v1.0/numbers', methods=['GET'])
 def get_numbers2():
     return jsonify({'numbers': numbers})
 
-@app.route('/api/v1.0/numbers', methods=['GET'])
-def get_numbers():
-    return jsonify({'numbers': numbers})
-
-@app.route('/api/v2.0/numbers/<string:e164>', methods=['GET'])
+@app.route('/api/v1.0/numbers/<string:e164>', methods=['GET'])
 def get_number2(e164):
     hget_number = hashlib.sha256()
     hget_number.update(bytes(e164, encoding='utf-8'))
@@ -46,7 +32,7 @@ def get_number2(e164):
     return jsonify({'Name': name})
 
 
-@app.route('/api/v2.0/numbers', methods=['POST'])
+@app.route('/api/v1.0/numbers', methods=['POST'])
 def create_number2():
     hget_number = hashlib.sha256()
     hget_number.update(bytes(request.json['e164'], encoding='utf-8'))
@@ -57,19 +43,6 @@ def create_number2():
 
     r.hset(hget_number.hexdigest(), 'name', request.json['name'])
     return jsonify({'Name': r.hget(hget_number.hexdigest(), 'name').decode('ascii')}), 201
-
-
-
-@app.route('/api/v1.0/numbers', methods=['POST'])
-def create_number():
-    if not request.json or not 'e164' in request.json:
-        abort(400)
-    number = {
-        'id': numbers[-1]['id'] + 1,
-        'e164': request.json['e164']
-    }
-    numbers.append(number)
-    return jsonify({'number': number}), 201
 
 @app.errorhandler(404)
 def not_found(error):
