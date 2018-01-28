ea67f52b84d6783173aadbace19e31107d862637
John Stafford <john@jkms.me>
2018-01-28 04:42:03 -0800

    Basic calling support

--- README.md
+++ README.md
@@ -37,3 +37,36 @@ Date: Sun, 28 Jan 2018 03:34:23 GMT
   "Name": "No One"
 }
 ```
+
+Adding a call:
+```
+$ curl -i -H "Content-Type: application/json" -X POST -d '{"e164":"+16048675309"}' http://localhost:5000/api/v1.0/calls
+HTTP/1.0 201 CREATED        
+Content-Type: application/json                          
+Content-Length: 49          
+Server: Werkzeug/0.14.1 Python/3.6.4                    
+Date: Sun, 28 Jan 2018 12:40:02 GMT                     
+
+{                           
+  "Time Stamp": "2018-01-28 04:40:02.432735"            
+}                           
+```
+
+Retrieving calls:
+```
+$ curl -i http://localhost:5000/api/v1.0/calls/+16048675309                               
+HTTP/1.0 201 CREATED        
+Content-Type: application/json                          
+Content-Length: 159         
+Server: Werkzeug/0.14.1 Python/3.6.4                    
+Date: Sun, 28 Jan 2018 12:40:50 GMT                     
+
+{                           
+  "Calls": [                
+    "2018-01-28 04:38:52.666644",                       
+    "2018-01-28 04:38:55.065851",                       
+    "2018-01-28 04:38:56.048959",                       
+    "2018-01-28 04:40:02.432735"                        
+  ]                         
+}                           
+```
--- phone.py
+++ phone.py
@@ -2,16 +2,27 @@ from flask import Flask, jsonify, abort, make_response, request
 app = Flask(__name__)
 # https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
 
+import json
 import redis
 import hashlib
+import time
+import datetime
+
 r = redis.Redis(
-    host='localhost'
+    host='localhost',
+    charset='utf-8'
 )
 m = hashlib.sha256()
 hash = hashlib.sha256()
 m.update(b'+16048675309')
 
 r.hset(m.hexdigest(), 'name', 'Jenny')
+
+def decode(l):
+    if isinstance(l, list):
+        return [decode(x) for x in l]
+    else:
+        return l.decode('utf-8')
 
 @app.route('/')
 def hello_world():
@@ -31,6 +42,15 @@ def get_number2(e164):
 #    return jsonify({'number': [number]})
     return jsonify({'Name': name})
 
+@app.route('/api/v1.0/calls/<string:e164>', methods=['GET'])
+def get_calls(e164):
+    he164 = hashlib.sha256()
+    he164.update(bytes(e164, encoding='utf-8'))
+    #e164calls = r.hvals('calls:' + he164.hexdigest())#.decode('ascii')
+    print('calls:' + he164.hexdigest())
+    calllist = decode(r.hvals('calls:' + he164.hexdigest()))
+    print(calllist)
+    return jsonify({'Calls': calllist}), 201
 
 @app.route('/api/v1.0/numbers', methods=['POST'])
 def create_number2():
@@ -43,6 +63,18 @@ def create_number2():
 
     r.hset(hget_number.hexdigest(), 'name', request.json['name'])
     return jsonify({'Name': r.hget(hget_number.hexdigest(), 'name').decode('ascii')}), 201
+
+@app.route('/api/v1.0/calls', methods=['POST'])
+def create_call():
+    he164 = hashlib.sha256()
+    he164.update(bytes(request.json['e164'], encoding='utf-8'))
+    if not r.exists(he164.hexdigest()):
+        abort(400)
+    if not request.json or not 'e164' in request.json:
+        abort(400)
+    calltime = time.time()
+    r.hset('calls:' + he164.hexdigest(), int(calltime), datetime.datetime.fromtimestamp(calltime))
+    return jsonify({'Time Stamp': r.hget('calls:' + he164.hexdigest(), int(calltime)).decode('ascii')}), 201
 
 @app.errorhandler(404)
 def not_found(error):
